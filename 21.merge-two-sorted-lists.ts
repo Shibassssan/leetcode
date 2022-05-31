fbdf7d739a3290c64feb32c99af4049bbb1a3cfd
Shibassssan <shibatayuichiworks@gmail.com>
2022-05-31 21:13:04 +0900

    21.merge-two-sorted-lists.ts 追加

---
+++ 21.merge-two-sorted-lists.ts
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=21 lang=typescript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const mergeTwoLists = (
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null => {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  const [first, second] =
+    list1.val > list2.val ? [list1, list2] : [list2, list1];
+  first.next = mergeTwoLists(first.next, second);
+
+  return first;
+};
+// @lc code=end
