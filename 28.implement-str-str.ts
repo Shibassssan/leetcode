8dead5d7a4b026dca430fa2e2b986108bf04ea04
Shibassssan <shibatayuichiworks@gmail.com>
2022-06-01 22:20:22 +0900

    28.implement-str-str.ts 解いた問題 追加

---
+++ 28.implement-str-str.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=28 lang=typescript
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+function strStr(haystack: string, needle: string): number {
+  return haystack.indexOf(needle);
+};
+// @lc code=end
+
