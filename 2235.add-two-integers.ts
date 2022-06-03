f8fc07afae856cc47fbb7b83fffb334898e021ed
Shibassssan <shibatayuichiworks@gmail.com>
2022-06-04 08:16:44 +0900

    2235.add-two-integers.ts 追加

---
+++ 2235.add-two-integers.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=2235 lang=typescript
+ *
+ * [2235] Add Two Integers
+ */
+
+// @lc code=start
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+};
+// @lc code=end
+
