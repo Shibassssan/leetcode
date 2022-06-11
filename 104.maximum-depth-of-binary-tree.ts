e0703b904aed02d7a61b1de4098ab6f588ce5303
Shibassssan <shibatayuichiworks@gmail.com>
2022-06-11 18:37:12 +0900

    add: 104.maximum-depth-of-binary-tree.ts

---
+++ 104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=104 lang=typescript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+// class TreeNode {
+//   val: number
+//   left: TreeNode | null
+//   right: TreeNode | null
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.left = (left===undefined ? null : left)
+//     this.right = (right===undefined ? null : right)
+//   }
+// }
+
+const maxDepth = (root: TreeNode | null): number => {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+// @lc code=end
+
