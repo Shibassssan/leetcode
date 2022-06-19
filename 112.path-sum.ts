84af33bea2ce25db97f93bb63fa4098b2055e44c
Shibassssan <shibatayuichiworks@gmail.com>
2022-06-20 08:41:25 +0900

    112.path-sum.ts 追加

---
+++ 112.path-sum.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=112 lang=typescript
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// class TreeNode {
+//   val: number
+//   left: TreeNode | null
+//   right: TreeNode | null
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.left = (left===undefined ? null : left)
+//     this.right = (right===undefined ? null : right)
+//   }
+// }
+
+const hasPathSum = (root: TreeNode | null, targetSum: number): boolean => {
+  if (root === null) return false;
+  if (!root.right && !root.left && root.val === targetSum) {
+    return true;
+  }
+  return hasPathSum(root.right, targetSum - root.val) || hasPathSum(root.left, targetSum - root.val);
+};
+// @lc code=end
+
