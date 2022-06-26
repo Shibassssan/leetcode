8c73407cf38b0d731e4f8533b7742d75d23c8bc2
Shibassssan <shibatayuichiworks@gmail.com>
2022-06-26 13:53:11 +0900

    [add]  155.min-stack.ts 解答追加

---
+++ 155.min-stack.ts
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=155 lang=typescript
+ *
+ * [155] Min Stack
+ */
+
+// @lc code=start
+class MinStack {
+  stack: number[];
+  constructor() {
+    this.stack = [];
+  }
+
+  push(val: number): void {
+    this.stack.push(val);
+  }
+
+  pop(): void {
+    this.stack.pop();
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return Math.min(...this.stack);
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
+
